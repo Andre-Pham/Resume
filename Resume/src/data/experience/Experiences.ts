eb9bd256620f76f2d28e73fb67e41a80370a09ec
Andre Pham <andrepham33@gmail.com>
2023-06-15 16:24:30 +1000

    Added remaining experiences

--- Resume/src/data/experience/Experiences.ts
+++ Resume/src/data/experience/Experiences.ts
@@ -21,7 +21,7 @@ export const Experiences: Experience[] = [
     new Experience(
         "Cerulean Labs",
         "Active",
-        "Part-Time Employment",
+        "Paid Positon",
         "2021 - Present",
         "As an iOS developer at Cerulean Labs, I've worked on all areas of the iPad Spaces app including the UI, rendering, feature development, and more. My CPD supporting document (linked below) provides are more thorough look at my contributions.",
         [
@@ -53,9 +53,9 @@ export const Experiences: Experience[] = [
 
     new Experience(
         "Fingerspell",
-        2023,
+        "Active",
         "Uni Individual Project",
-        "2023",
+        "2023 - Present",
         "An iOS app that can convert a text prompt into an animated, interactive 3D model performing the prompt in Auslan fingerspelling, as well as the recognition and translation of fingerspelling performed by the user through their camera back into text.",
         [
             new Link("GitHub", "GitHub.com/Andre-Pham/SpellApp", "https://github.com/Andre-Pham/SpellApp")
@@ -95,6 +95,66 @@ export const Experiences: Experience[] = [
         [],
         [
             "Swift", "UIKit", "MVC", "Core Data", "UNUserNotificationCenter"
+        ]
+    ),
+
+    new Experience(
+        "Intake",
+        "Active",
+        "Uni Team Project",
+        "2023 - Present",
+        "A multi-platform mental heatth triage app. Allows for nurses to triage patients, view and append to patient history, and view other triage cases. Allows admins to manage accounts and export patient data, and allows nurse leaders to manage shift turnover and manage patient allocation. Has account login, live data, and supports mobile, tablet, and web.",
+        [
+            new Link("GitHub", "[Unavailable until publication made]", "https://github.com/FIT4002-TEAM9/Leaf")
+        ],
+        [],
+        [
+            "React Native Expo", "Typescript", "Redux", "MongoDB"
+        ]
+    ),
+
+    new Experience(
+        "Information Visualiser",
+        2021,
+        "Personal Project",
+        "2020 - 2021",
+        "A self-made QR code program that can turn text into visual pattern, and back again. Allows the creation and saving of a 'VISREP', as well as the recognition of them via image file or live video steam.",
+        [
+            new Link("GitHub", "GitHub.com/Andre-Pham/Information-Visualiser", "https://github.com/Andre-Pham/Information-Visualiser")
+        ],
+        [],
+        [
+            "Python", "Tkinter", "cv2", "NumPy", "PIL"
+        ]
+    ),
+
+    new Experience(
+        "Dynamic Tic-Tac-Toe",
+        2021,
+        "Personal Project",
+        "2021",
+        "A Tic-tac-toe game in which the computer plays the user and either draws or wins. The user decides how many columns, rows, and how many in a row to win, though processing is limited to a board with 20 spaces. Implements lexicographic permutation for computer decisions.",
+        [
+            new Link("GitHub", "GitHub.com/Andre-Pham/Dynamic-Tic-Tac-Toe", "https://github.com/Andre-Pham/Dynamic-Tic-Tac-Toe")
+        ],
+        [],
+        [
+            "Python", "Tkinter"
+        ]
+    ),
+
+    new Experience(
+        "Vision",
+        "Active",
+        "Paid Positon",
+        "2023 - Present",
+        "In progress. More to come...",
+        [
+            new Link("GitHub", "GitHub.com/Andre-Pham/LemonApp", "https://github.com/Andre-Pham/LemonApp")
+        ],
+        [],
+        [
+            "Swift", "UIKit", "MVC", "Create ML", "Vision", "AVFoundation", "Speech"
         ]
     ),
 ];
