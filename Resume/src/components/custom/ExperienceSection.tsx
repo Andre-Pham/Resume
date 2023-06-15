d54aceeb0143f99aa6e02b63550d50b27b8d446a
Andre Pham <andrepham33@gmail.com>
2023-06-15 11:39:38 +1000

    Cleaned up ExperienceSection

--- Resume/src/components/custom/ExperienceSection.tsx
+++ Resume/src/components/custom/ExperienceSection.tsx
@@ -18,6 +18,7 @@ const ExperienceSection: React.FC<Props> = ({
     period,
 }) => {
     const [screenIsPortrait, setScreenIsPortrait] = useState(Environment.instance.getScreenWidth() <= 950);
+    const [componentWidth, setComponentWidth] = useState(StateManager.contentWidth.read());
 
     useEffect(() => {
         Dimensions.addEventListener('change', (newDimensions) => {
@@ -25,48 +26,26 @@ const ExperienceSection: React.FC<Props> = ({
         });
     }, []);
 
-    const [dimensions, setDimensions] = useState({ 
-        width: StateManager.contentWidth.read(), 
-        height: 0
+    StateManager.contentWidth.subscribe(() => {
+        setComponentWidth(StateManager.contentWidth.read());
     });
+
     let columnCount = screenIsPortrait ? 1 : 2;
     let spacing = 20;
     let gap = (columnCount - 1)*spacing;
-
-    // For some reason onLayout doesn't work at smaller dimensions
-    // Ideally we'd read the container component's dimensions, not the screen
-    // This is a workaround
-    // useEffect(() => {
-    //     Dimensions.addEventListener('change', (newDimensions) => {
-    //         setDimensions({
-    //             width: 0, 
-    //             height: 50,
-    //         });
-    //     });
-    // }, []);
-
-    StateManager.contentWidth.subscribe(() => {
-        console.log("RECEIVED: " + StateManager.contentWidth.read());
-        setDimensions({
-            width: StateManager.contentWidth.read(), 
-            height: 50,
-        });
-    });
-
-    // const [width, setWidth] = useState(StateManager.contentWidth.read());
 
     const renderExperienceCards = () => {
         return period.experiences.map((experience) => (
             <ExperienceCard 
                 key={experience.name} 
                 experience={experience} 
-                style={{ flex: null, width: (dimensions.width - gap) / columnCount }} 
+                style={{ flex: null, width: (componentWidth - gap) / columnCount }} 
             />
         ));
     };
 
     return (
-        <VStack spacing={24} style={{ width: dimensions.width }}>
+        <VStack spacing={24} style={{ width: componentWidth }}>
             <YearHeader>
                 {period.label}
             </YearHeader>
