9d76cb22e20a90268cdf027f0cb7ebd748b5b7a0
Andre Pham <andrepham33@gmail.com>
2024-05-03 12:13:12 +1000

    handle resize is now called on load

--- react/Resume/src/components/hooks/usePortraitRendering.ts
+++ react/Resume/src/components/hooks/usePortraitRendering.ts
@@ -7,6 +7,7 @@ function usePortraitRendering(callback: (shouldRenderPortrait: boolean) => void)
             const shouldRenderPortrait = window.innerWidth <= ResDimensions.screenWidthToRenderPortrait;
             callback(shouldRenderPortrait);
         };
+        handleResize();
         // When the window is resized, re-update
         window.addEventListener("resize", handleResize);
         // Cleanup the event listener on component unmount
