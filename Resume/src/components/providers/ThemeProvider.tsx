51673c002f84a51bda50eae9c765e0db26f55ee3
Andre Pham <andrepham33@gmail.com>
2024-05-07 20:49:07 +1000

    Added styled light mode and dark mode scroll bars

--- Resume/src/components/providers/ThemeProvider.tsx
+++ Resume/src/components/providers/ThemeProvider.tsx
@@ -2,6 +2,8 @@ import React, { createContext, useState, useEffect, useMemo } from "react";
 import StateManager from "../../state/publishers/StateManager";
 import ResColors from "../styling/ResColors";
 import LocalStorageManager from "../../services/LocalStorageManager";
+import { createGlobalStyle } from "styled-components";
+import { ColorScheme } from "../../state/publishers/types/ColorScheme";
 
 const ColorThemeContext = createContext(StateManager.colorScheme.read());
 
@@ -28,5 +30,38 @@ export const ColorThemeProvider: React.FC<Props> = ({ children }) => {
         LocalStorageManager.inst.writeColorTheme(colorScheme);
     }, [colorScheme]);
 
-    return <ColorThemeContext.Provider value={value.colorScheme}>{children}</ColorThemeContext.Provider>;
+    return (
+        <ColorThemeContext.Provider value={value.colorScheme}>
+            <GlobalStyle colorScheme={colorScheme} />
+            {children}
+        </ColorThemeContext.Provider>
+    );
 };
+
+// Color scheme still has to be passed in to react
+const GlobalStyle = createGlobalStyle<{ colorScheme: ColorScheme }>`
+    body {
+        scrollbar-color: ${() => `${ResColors.textSemiDark.getColor()} ${ResColors.fillBackgroundLight.getColor()}`};
+        transition: background-color 0.2s ease, color 0.2s ease;
+    }
+
+    ::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${() => ResColors.textSemiDark.getColor()};
+        border-radius: 0px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: ${() => ResColors.textSemiDark.getColor()};
+        border-radius: 0px;
+        
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: ${() => ResColors.fillBackgroundLight.getColor()};
+        border-radius: 0px;
+    }
+`;
