751cc5b6a592c8f1da74548be94e10bdfe03005e
Andre Pham <andrepham33@gmail.com>
2026-06-02 03:12:42 +1000

    design

--- kiwi/src/pages/home-page.tsx
+++ kiwi/src/pages/home-page.tsx
@@ -10,28 +10,31 @@ export function HomePage() {
       <div className="flex min-h-[80svh] flex-1 flex-col items-center p-6">
         <div className="min-h-0 flex-1" />
 
-        <div>
-          <p className="font-plus-jakarta-sans text-[64px] font-black">
+        <div className="w-fit max-w-full">
+          <p className="font-plus-jakarta-sans max-w-full text-[32px] leading-9 font-black min-[460px]:hidden">
+            Andre Pham
+          </p>
+          <p className="font-plus-jakarta-sans hidden max-w-full leading-none font-black whitespace-nowrap min-[460px]:block min-[460px]:text-[48px] sm:text-[64px]">
             andre<span className="italic">pham</span>.com
           </p>
-          <p className="font-cal-sans-text mt-2 max-w-md text-[18px] font-medium">
+          <p className="font-cal-sans-text mt-6 max-w-lg text-[18px] font-medium">
             I’m a mobile and full stack developer who specialises in native iOS,
             Node.js, and React.
           </p>
 
           <Separator className="mt-8" />
 
-          <div className="flex items-center gap-4">
-            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+          <div className="mt-8 flex flex-wrap items-center gap-3 sm:gap-4">
+            <p className="font-gitlab-mono bg-accent w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
               Showcase
             </p>
-            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+            <p className="font-gitlab-mono bg-accent w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
               Career
             </p>
-            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+            <p className="font-gitlab-mono bg-accent w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
               Socials
             </p>
-            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+            <p className="font-gitlab-mono bg-accent w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
               Education
             </p>
           </div>
