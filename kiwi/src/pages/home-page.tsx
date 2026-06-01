208fa119e4c4aa0509163df85cfc90c1b6b8b33d
Andre Pham <andrepham33@gmail.com>
2026-06-02 02:23:33 +1000

    design

--- kiwi/src/pages/home-page.tsx
+++ kiwi/src/pages/home-page.tsx
@@ -7,48 +7,38 @@ export function HomePage() {
       {/* <h1>Hello World</h1>
       <p>{theme}</p>
       <Button onClick={toggleTheme}>Toggle</Button> */}
-      <div className="mx-auto mt-24 max-w-4xl">
-        <p className="font-plus-jakarta-sans text-[64px] font-black">
-          andre<span className="italic">pham</span>.com
-        </p>
-        {/* <p className="font-gentium-plus max-w-sm text-[22px] font-bold">
-          I’m a mobile and full stack developer who specialises in native iOS,
-          Node.js, and React.
-        </p> */}
-        <p className="font-cal-sans-text mt-2 max-w-md text-[18px] font-medium">
-          I’m a mobile and full stack developer who specialises in native iOS,
-          Node.js, and React.
-        </p>
+      <div className="flex min-h-[80svh] flex-1 flex-col items-center p-6">
+        <div className="min-h-0 flex-1" />
 
-        <Separator className="mt-8" />
+        <div>
+          <p className="font-plus-jakarta-sans text-[64px] font-black">
+            andre<span className="italic">pham</span>.com
+          </p>
+          <p className="font-cal-sans-text mt-2 max-w-md text-[18px] font-medium">
+            I’m a mobile and full stack developer who specialises in native iOS,
+            Node.js, and React.
+          </p>
 
-        <div className="flex items-center gap-4">
-          <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[16px]">
-            Showcase
-          </p>
-          <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[16px]">
-            Career
-          </p>
-          <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[16px]">
-            Socials
-          </p>
-          <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[16px]">
-            Education
-          </p>
+          <Separator className="mt-8" />
+
+          <div className="flex items-center gap-4">
+            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+              Showcase
+            </p>
+            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+              Career
+            </p>
+            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+              Socials
+            </p>
+            <p className="font-gitlab-mono bg-accent mt-8 w-min rounded-lg px-3 py-1 text-[14px] font-semibold">
+              Education
+            </p>
+          </div>
         </div>
 
-        {/* <p className="font-gitlab-mono mt-8 text-[16px]">Showcase</p>
-
-        <p className="font-gitlab-mono mt-2 text-[16px]">Career</p>
-
-        <p className="font-gitlab-mono mt-2 text-[16px]">Socials</p>
-
-        <p className="font-gitlab-mono mt-2 text-[16px]">Education</p> */}
-
-        {/* <p className="font-cal-sans-text mx-auto mt-8 max-w-sm text-[15px] leading-5 font-normal">
-          I’m a mobile and full stack developer who specialises in native iOS,
-          Node.js, and React.
-        </p> */}
+        <div className="h-6" />
+        <div className="min-h-0 flex-1" />
       </div>
     </div>
   )
