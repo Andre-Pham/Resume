94d7edca1ce42cd843e3ad517f2b5764e51b5b67
Andre Pham <andrepham33@gmail.com>
2026-06-13 20:39:08 +1000

    eina

--- kiwi/src/pages/career-page.tsx
+++ kiwi/src/pages/career-page.tsx
@@ -1,3 +1,23 @@
+import { Separator } from "@/components/ui/separator"
+import { Text } from "@/components/ui/text"
+
 export function CareerPage() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <div className="mt-12">
+        <Text variant="display" className="text-center">
+          Career
+        </Text>
+      </div>
+
+      <div className="mt-16 w-full max-w-xl">
+        <Text variant="h3">Bachelor of Software Engineering (Honours)</Text>
+
+        <Text variant="mono" className="text-muted-foreground mt-2">
+          Monash University, Mar 2020 – Nov 2023
+        </Text>
+        <Separator className="mt-7" />
+      </div>
+    </div>
+  )
 }
